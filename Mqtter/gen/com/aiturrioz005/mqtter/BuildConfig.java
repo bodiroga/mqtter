e8b1b9b27e97d16caa294583dd994a2fa8306ba0
Aitor <aiturrioz005@ikasle.ehu.es>
2014-10-05 22:27:19 +0200

    First version of the plugin

---
+++ Mqtter/gen/com/aiturrioz005/mqtter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.aiturrioz005.mqtter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Mqtter/src/com/aiturrioz005/mqtter/BundleExtraKeys.java
@@ -0,0 +1,13 @@
+package com.aiturrioz005.mqtter;
+
+public class BundleExtraKeys {
+	
+	public static final String SERVER = "Server";
+	public static final String PORT = "Port";
+	public static final String USERNAME = "Username";
+	public static final String PASSWORD = "Password";
+	public static final String TOPIC = "Topic";
+	public static final String PAYLOAD = "Payload";
+
+
+}
